6475928dfda2a8d0a21b91b1a82e93fe99005231
dalek-who <839991169@qq.com>
2020-05-06 10:21:03 +0800

    gym原版的Pendulum-v0可以了，但是我自定义的倒立摆还是不能稳定在上部

--- DQN_myenv.py
+++ DQN_myenv.py
@@ -4,17 +4,25 @@
 import matplotlib
 # matplotlib.use('TkAgg')
 
+import gym
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torch.utils.data import Dataset, DataLoader, TensorDataset
+from torch.utils.data import Dataset, DataLoader, TensorDataset, RandomSampler
 from collections import deque
 from tqdm import tqdm
+from collections import namedtuple
+from time import sleep
 
 from BaseAgent import BaseAgent
+from game.my_Pendulum import MyPendulumEnv
 
+torch.manual_seed(2)
+np.random.seed(2)
+
+Transition = namedtuple('Transition', ['state', 'feature', 'index_action', 'reward', 'next_state', 'next_feature'])
 
 class DQN_Net(nn.Module):
     def __init__(self, n_features, n_actions):
@@ -32,14 +40,31 @@ class DQN_Net(nn.Module):
         return self.net(x)
 
 
+class DQN_Dataset(Dataset):
+    def __init__(self, example_list):
+        super(DQN_Dataset, self).__init__()
+        self.example_list = example_list
+
+    def __len__(self):
+        return len(self.example_list)
+
+    def __getitem__(self, item):
+        return self.example_list[item]
+
+
+def state_to_feature(state):
+    return np.array([np.sin(state[0]), np.cos(state[0]), state[1]])
+
 
 class DQN_Agent(BaseAgent):
     def __init__(self, n_theta=200, n_d_theta=201, n_actions=3, gamma=0.98):
         super(DQN_Agent, self).__init__(n_theta, n_d_theta, n_actions, gamma)
         self.q_table = None
+        self.env = MyPendulumEnv()
 
     # 通过epsilon贪心策略返回一个action
     def epsilon_greedy_action(self, feature, epsilon, return_index=False):
+        feature = torch.from_numpy(feature).float().unsqueeze(0)
         if np.random.rand() > 1-epsilon:  # 以1-epsilon的概率返回q最大的action
             with torch.no_grad():
                 index_action = self.target_net(torch.tensor(feature).float())
@@ -52,6 +77,7 @@ class DQN_Agent(BaseAgent):
         return index_action if return_index else action
 
     def do_action(self, feature, return_index=False):
+        feature = torch.from_numpy(feature).float().unsqueeze(0)
         with torch.no_grad():
             index_action = self.target_net(torch.tensor(feature).float())
             index_action = index_action.argmax()
@@ -67,7 +93,7 @@ class DQN_Agent(BaseAgent):
 
     def train(self):
         queue_size = 2000
-        num_epoch = 100
+        num_epoch = 5000
         episode_len = 200
         batch_size = 32
         max_epsilon = 1
@@ -75,7 +101,7 @@ class DQN_Agent(BaseAgent):
         epsilon_decay = 0.999
         max_grad_norm = 0.5
         lr = 1e-3
-        n_features = 4
+        n_features = 3
 
         self.policy_net: nn.Module = DQN_Net(n_actions=self.n_actions, n_features=n_features)
         self.target_net: nn.Module = DQN_Net(n_actions=self.n_actions, n_features=n_features)
@@ -93,41 +119,35 @@ class DQN_Agent(BaseAgent):
             score = 0
             state = self.env.reset()
             for episode_step in range(episode_len):  # 采样新的episode
-                feature = (state[0], state[1], np.sin(state[0]), np.cos(state[0]))  # theta, d_theta, sin(theta), cos(theta)
+                feature = state_to_feature(state)  # theta, d_theta, sin(theta), cos(theta)
                 index_action = self.epsilon_greedy_action(feature, epsilon=epsilon, return_index=True)  # epsilon-greedy采用action
                 next_state, reward, done, info = self.env.step(np.array([self.actions[index_action]]))
-                theta, d_theta = state
-                next_theta, next_d_theta = next_state
-                # example = (theta, d_theta, index_action, reward, next_theta, next_d_theta)
-                example = (theta, d_theta, np.sin(theta), np.cos(theta),  index_action, reward, next_theta, next_d_theta, np.sin(next_theta), np.cos(next_theta))
+                next_fearure = state_to_feature(next_state)
+                example = Transition(state, feature, np.array([index_action]), np.array([reward]), next_state, next_fearure)
                 examples_queue.append(example)
                 score += reward
-                self.env.render()
                 if done:
-                    # theta, d_theta = self.env.reset()
                     state = self.env.reset()
                 else:
-                    # theta, d_theta = next_theta, next_d_theta
                     state = next_state
-            if len(examples_queue)<examples_queue.maxlen:
-                continue
-            dataloader = DataLoader(TensorDataset(torch.tensor(examples_queue)), batch_size=batch_size)
+                if epoch % 50 == 0:
+                    self.env.render()
+            dataset = DQN_Dataset(example_list=examples_queue)
+            dataloader = DataLoader(dataset, batch_size=batch_size, sampler=RandomSampler(dataset))
             for data in dataloader:
                 global_step += 1
-                data = data[0]
-                # tensor_state = data[:, [0,1]].float()  # 当前state，[theta, d_theta]
-                # tensor_index_action = data[:, [2]].long()  # 当前state依据epsilon-greedy采样的策略index
-                # tensor_reward = data[:, [3]].float()  # 当前(state, action)的reward
-                # tensor_next_state = data[:, [4,5]].float()  # 下一个state
-                tensor_state = data[:, [0, 1, 2, 3]].float()  # 当前state，[theta, d_theta]
-                tensor_index_action = data[:, [4]].long()  # 当前state依据epsilon-greedy采样的策略index
-                tensor_reward = data[:, [5]].float()  # 当前(state, action)的reward
-                tensor_next_state = data[:, [6, 7, 8, 9]].float()  # 下一个state
 
-                tensor_state_q_values = self.policy_net(tensor_state)  # 当前state下，神经网络拟合出的每个action的q_value
+                tensor_state = data.state.float()  # 当前state，[theta, d_theta]
+                tensor_feature = data.feature.float()
+                tensor_index_action = data.index_action.long()  # 当前state依据epsilon-greedy采样的策略index
+                tensor_reward = data.reward.float()  # 当前(state, action)的reward
+                tensor_next_state = data.next_state.float()  # 下一个state
+                tensor_next_feature = data.next_feature.float()
+
+                tensor_state_q_values = self.policy_net(tensor_feature)  # 当前state下，神经网络拟合出的每个action的q_value
                 tensor_state_action_q_value = tensor_state_q_values.gather(dim=1, index=tensor_index_action)  # 当前state对应的epsilon-greedy action的，神经网络拟合的q_value
                 with torch.no_grad():
-                    tensor_next_state_q_values = self.target_net(tensor_next_state)  # 下个state下，神经网络拟合出的每个action的q_value
+                    tensor_next_state_q_values = self.target_net(tensor_next_feature)  # 下个state下，神经网络拟合出的每个action的q_value
                 tensor_expected_state_action_q_value = (tensor_next_state_q_values.max(dim=1, keepdim=True)[0] * self.gamma) + tensor_reward  # 依据greedy策略，当前state期望的q_value
                 loss = F.smooth_l1_loss(tensor_state_action_q_value, tensor_expected_state_action_q_value)
 
@@ -141,18 +161,41 @@ class DQN_Agent(BaseAgent):
                     self.target_net.load_state_dict(self.policy_net.state_dict())
                 with torch.no_grad():
                     q = tensor_state_action_q_value.mean().item()
+                    running_q = 0.99 * running_q + 0.01 * q
+                reward_list.append(loss.item())
+
             running_reward = running_reward * 0.9 + score * 0.1
-            reward_list.append(score)
+            if epoch % 10 == 0:
+                print('Ep {}\tAverage score: {:.2f}\tAverage Q: {:.2f}'.format(
+                    epoch, running_reward, running_q))
             if running_reward>-200:
                 break
         self.show_convergence_curve(reward_list)
+
+    def demo(self, max_step=1000):
+        observation = self.env.reset()
+        self.env.render()
+        sleep(1)
+        step = 0
+        while True:
+            step += 1
+            self.env.render()  # 渲染动画
+            feature = state_to_feature(observation)
+            action = self.do_action(feature)
+            observation, reward, done, info = self.env.step(action)
+            if done or step>max_step:
+                print("Episode finished after {} timesteps, done={}".format(step + 1, done))
+                break
+        sleep(2)
+        self.env.close()
 
 
 
 
 agent = DQN_Agent()
 agent.train()
-agent.demo()
+for i in range(10):
+    agent.demo()
 
 
 
